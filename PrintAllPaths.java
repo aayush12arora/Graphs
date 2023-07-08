3ae3de515b6edab8f6a38b7583c46b22282ca536
Aayush <aayush12arora@gmail.com>
2023-07-08 20:22:50 +0530

    All Paths from Source to destination

--- PrintAllPaths.java
+++ PrintAllPaths.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class IfGraphIsNotContinuous {
+public class PrintAllPaths {
         static class Edge{
         int src;
         int dest;
@@ -23,8 +23,10 @@ graph[1].add(new Edge(1, 3));
 graph[2].add(new Edge(2, 0));
 graph[2].add(new Edge(2,4));
 graph[3].add(new Edge(3, 1));
+graph[3].add(new Edge(3, 4));
 graph[3].add(new Edge(3, 5));
 graph[4].add(new Edge(4, 2));
+graph[4].add(new Edge(4, 3));
 graph[4].add(new Edge(4, 5));
 graph[5].add(new Edge(5, 3));
 graph[5].add(new Edge(5, 4));
@@ -32,10 +34,10 @@ graph[5].add(new Edge(5, 6));
 graph[6].add(new Edge(6, 5));
     }
 
-    public static void bfs(ArrayList<Edge> graph [],boolean visited[],int start){
+    public static void bfs(ArrayList<Edge> graph []){
         Queue<Integer> q = new LinkedList<>();
-     
-        q.add(start);
+        boolean visited []= new boolean[graph.length];
+        q.add(graph[0].get(0).src);
         while(!q.isEmpty()){
 int curr = q.remove();
 if(!visited[curr]){
@@ -51,15 +53,43 @@ for (int i = 0; i < graph[curr].size(); i++) {
 
     }
 
-    public static void dfs(ArrayList<Edge> graph[], boolean visited[], int curr) {
-        if (!visited[curr]) {
-            visited[curr] = true;
-            System.out.print(curr + " ");
-          
-            for (int i = 0; i < graph[curr].size(); i++) {
-                Edge e = graph[curr].get(i); 
+    public static void dfs (ArrayList<Edge> graph[],boolean visited[],int curr){
+        if(visited[curr]==false){
+        System.out.print(curr+" ");
+               visited[curr]=true;
+      
+            for(int i=0;i<graph[curr].size();i++){
+                
+         
+                Edge e = graph[curr].get(i);
+
                 dfs(graph, visited, e.dest);
+
+            
+        }
+    }
+    }
+
+
+    public static void modifieddfs(ArrayList<Edge> graph[], boolean visited[], int curr, int tar, String path) {
+        if (curr == tar) {
+            System.out.println(path);
+            return;
+        }
+    
+      
+             if(!visited[curr]){
+    
+            for (int i = 0; i < graph[curr].size(); i++) {
+                Edge e = graph[curr].get(i);
+              
+              
+                     visited[curr] = true;
+                modifieddfs(graph, visited, e.dest, tar, path + e.dest);
+                    visited[curr] =false; 
             }
+    
+        
         }
     }
     
@@ -94,19 +124,15 @@ for (int i = 0; i < graph[curr].size(); i++) {
         }
 
         System.out.println("Graph with BFS");
- boolean vis[]= new boolean[V];
-        for ( int i = 0; i < V; i++) {
-             bfs(graph,vis,i);
-        }
-       
+
+        bfs(graph);
 
         boolean visited[]= new boolean[V];
         System.out.println();
-          System.out.println("Graph with DFS if graph is broken into parts");
+        //   System.out.println("Graph with DFS");
+        // dfs(graph, visited, graph[0].get(0).src);
 
-          for (int i = 0; i <V; i++) {
-            dfs(graph, visited, i); // passing it by makig every vertex as  a starting point
-          }
-       
+          System.out.println("All paths from 0 to 5 are");
+        modifieddfs(graph, visited, 0,5,"0");
     }
 }
